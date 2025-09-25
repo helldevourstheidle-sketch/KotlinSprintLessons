c61dc294c29d6f038e0877c635f0941b2c99bc96
AceVintura <helldevourstheidle@gmail.com>
2025-09-25 21:11:25 +0300

    KS-2-2 create variable employeesCount

--- src/main/kotlin/lesson_2/lesson2-task-1.kt
+++
@@ -1,8 +0,0 @@
-package org.example.lesson_2
-
-fun main() {
-    var ordersCount:Int=75 //var - т.к переменную с кол-вом заказов подразумеваю как динамическую
-    val msgAfterOrder:String="Thx for order, waiting you again..." //val - т.к. текст будет статикой и меняться не будет, только вопрос как это отразится на локализации ... В момент потребности в переводах может ли это стать проблемой ?
-println(ordersCount)
-    println(msgAfterOrder) //вывел в консоль для перепроверки
-}
