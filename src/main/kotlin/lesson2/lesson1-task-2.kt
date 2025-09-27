cbe5506e9d19b2ca3cd1a8e1994336dcc556428d
AceVintura <helldevourstheidle@gmail.com>
2025-09-27 10:28:37 +0300

    KS-1-homeTask 2

---
+++ src/main/kotlin/lesson2/lesson1-task-2.kt
@@ -0,0 +1,12 @@
+package org.example.lesson2
+
+fun main() {
+    var ordersCount: Int = 75
+    val msgAfterOrder: String = "Thx for order, waiting you again..."
+    println(ordersCount)
+    println(msgAfterOrder)
+    var employeesCount: Int = 2000
+    //println(employeesCount)
+    employeesCount = 1999
+    println(employeesCount)
+}
