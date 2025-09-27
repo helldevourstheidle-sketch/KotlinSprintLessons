20f98ee92be39645344a6f38e77536862ef8da17
AceVintura <helldevourstheidle@gmail.com>
2025-09-27 20:36:36 +0300

    KS-3-2 task3 Train Arrival Time Counter

---
+++ src/main/kotlin/lesson2/lesson2-task-4.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val buff = 20
+    val metalOre = 11
+    val crystalOre = 7
+    val buffMetalOre: Int = metalOre * buff / 100
+    val buffCrystalOre: Int = crystalOre * buff / 100
+    println(buffMetalOre)
+    println(buffCrystalOre)
+}
