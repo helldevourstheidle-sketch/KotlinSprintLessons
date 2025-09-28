5f0cbd94a708406a64897f0b6d4d656373fdcb64
AceVintura <helldevourstheidle@gmail.com>
2025-09-28 19:16:17 +0300

    KS-3-2 remark

--- src/main/kotlin/lesson2/lesson2-task3.kt
+++ src/main/kotlin/lesson2/lesson2-task3.kt
@@ -2,13 +2,13 @@ fun main() {
     val trainLeftTimeHours = 9
     val trainLeftTimeMinutes = 39
     val travelTimeMinutes = 457
-    val travelTimeHours = travelTimeMinutes / minInHour
-    val travelTimeHoursRemain = travelTimeMinutes % minInHour
-    val travelTimeEndHours = (trainLeftTimeMinutes + travelTimeHoursRemain) / minInHour
-    val travelTimeEndMinutes = (trainLeftTimeMinutes + travelTimeHoursRemain) % minInHour
+    val travelTimeHours = travelTimeMinutes / MIN_IN_HOUR
+    val travelTimeHoursRemain = travelTimeMinutes % MIN_IN_HOUR
+    val travelTimeEndHours = (trainLeftTimeMinutes + travelTimeHoursRemain) / MIN_IN_HOUR
+    val travelTimeEndMinutes = (trainLeftTimeMinutes + travelTimeHoursRemain) % MIN_IN_HOUR
     print(trainLeftTimeHours + travelTimeHours + travelTimeEndHours)
     print(':')
     print(travelTimeEndMinutes)
 }
 
-const val minInHour = 60
+const val MIN_IN_HOUR = 60
