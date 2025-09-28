662351ba30f5044dfccd76cb40e2ae5dcd9b7dee
AceVintura <helldevourstheidle@gmail.com>
2025-09-28 19:11:53 +0300

    KS-2-3 user data before/after marriage

---
+++ src/main/kotlin/lesson3/KS-2-3.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val firstName = "Татьяна"
+    var lastNameBeforeMarriage = "Андреева"
+    var lastNameAfterMarriage = "Сидорова"
+    val patronymic = "Сергеевна"
+    var ageBeforeMarriage = 20
+    var ageAfterMarriage = 22
+    var space = ' '
+    println(lastNameBeforeMarriage + space + firstName + space + patronymic + ',' + ageBeforeMarriage)
+    println(lastNameAfterMarriage + space + firstName + space + patronymic + ',' + ageAfterMarriage)
+
+}
