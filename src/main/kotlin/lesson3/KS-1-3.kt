72b01e712c2835b490b2b3cf5306cb95d9ca8801
AceVintura <helldevourstheidle@gmail.com>
2025-09-28 18:52:11 +0300

    KS-1-3 task1 Say hello to user

---
+++ src/main/kotlin/lesson3/KS-1-3.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val helloDay = "Добрый день"
+    val helloNight = "Добрый вечер"
+    val userName = "Джон маклейн"
+    val helloUserDay = "$helloDay $userName"
+    val helloUserNight = "$helloNight $userName"
+    println(helloUserDay)
+    println(helloUserNight)
+
+}
