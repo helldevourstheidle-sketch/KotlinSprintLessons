3de0e5455c8569549f83574702812f9b98473283
AceVintura <helldevourstheidle@gmail.com>
2025-09-28 18:26:25 +0300

    KS-5-1 task5 remark

--- src/main/kotlin/lesson1/lesson1-task-5.kt
+++ src/main/kotlin/lesson1/lesson1-task-5.kt
@@ -1,12 +1,11 @@
 fun main() {
     val secInSpace: Short = 6480
-    val minInSpace = secInSpace / secInMin
-    val hourInSpace = minInSpace / minInHour
-    val ostMinInSpace = secInSpace % secInMin
-    val ostHourInSpace = minInSpace % minInHour
-    val timeInSpaceToPrint = String.format("%s$hourInSpace:$ostHourInSpace:%s$ostMinInSpace", "0", "0")
-    println(timeInSpaceToPrint)
+    val minInSpace = secInSpace / SEC_IN_MIN
+    val hourInSpace = minInSpace / MIN_IN_HOUR
+    val ostMinInSpace = secInSpace % SEC_IN_MIN
+    val ostHourInSpace = minInSpace % MIN_IN_HOUR
+    println("%02d:%02d:%02d".format(hourInSpace, ostHourInSpace, ostMinInSpace))
 }
 
-const val minInHour = 60
-const val secInMin = 60
+const val MIN_IN_HOUR = 60
+const val SEC_IN_MIN = 60
