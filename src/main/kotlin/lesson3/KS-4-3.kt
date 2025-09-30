93b0861f454b9d582bb3c8a01327592a71fd87fd
AceVintura <helldevourstheidle@gmail.com>
2025-09-30 21:53:51 +0300

    KS-3-4 conditions for the growth of plants

--- src/main/kotlin/lesson3/KS-4-3.kt
+++ src/main/kotlin/lesson3/KS-4-3.kt
@@ -2,11 +2,9 @@ fun main() {
     var stepNumber = 1
     var stepFrom = "E2"
     var stepTo = "E4"
-    var stepBlack = stepFrom + '-' + stepTo + ';' + stepNumber
-    println(stepBlack)
+    println("$stepFrom-$stepTo;$stepNumber")
     stepNumber = 2
     stepFrom = "D2"
     stepTo = "D3"
-    var stepWhite = stepFrom + '-' + stepTo + ';' + stepNumber
-    println(stepWhite)
+    println("$stepFrom-$stepTo;$stepNumber")
 }
