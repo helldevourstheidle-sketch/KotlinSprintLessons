b3ee0168a2b755805d765d39486e22aef185954b
AceVintura <helldevourstheidle@gmail.com>
2025-10-02 22:18:12 +0300

    KS-5-4 Исправил

--- src/main/kotlin/lesson4/KS-5-4.kt
+++ src/main/kotlin/lesson4/KS-5-4.kt
@@ -7,5 +7,15 @@ fun main() {
     val provisionsCount = readln().toInt()
     println("Погода благоприятна true/false??")
     val isWeatherGood = readLine().toBoolean()
-    println("Корабль может выходить в плавание?:${hasDamage == false && crewCount >= 55 && crewCount <= 70 && provisionsCount > 50 && isWeatherGood == true || crewCount > 70 && isWeatherGood == true && provisionsCount > 50}")
+    println("Корабль может выходить в плавание?:${hasDamage == HAS_DAMAGE_FALSE && crewCount >= CREW_COUNT_55 && crewCount <= CREW_COUNT_70 && provisionsCount > PROVISION_COUNT_50 && isWeatherGood == IS_WEATHER_GOOD_TRUE || crewCount > CREW_COUNT_70 && isWeatherGood == IS_WEATHER_GOOD_TRUE && provisionsCount > PROVISION_COUNT_50}")
 }
+
+const val HAS_DAMAGE_FALSE = false
+const val CREW_COUNT_55 = 55
+const val CREW_COUNT_70 = 70
+const val PROVISION_COUNT_50 = 50
+const val IS_WEATHER_GOOD_TRUE = true
+
+
+
+
