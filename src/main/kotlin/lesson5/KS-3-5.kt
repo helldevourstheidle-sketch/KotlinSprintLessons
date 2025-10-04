fa755e8046e7d27ff32a74cb317fd993b745cf2a
AceVintura <helldevourstheidle@gmail.com>
2025-10-04 17:06:25 +0300

    KS-3-5 lottery

---
+++ src/main/kotlin/lesson5/KS-3-5.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val winNumber1 = 10
+    val winNumber2 = 20
+    println("Введите первое число от 0 до 42")
+    val userNumber1 = readln().toInt()
+    println("Введите второе число от 0 до 42")
+    val userNumber2 = readln().toInt()
+    if (userNumber1 == winNumber1 && userNumber2 == winNumber2 || userNumber1 == winNumber2 && userNumber2 == winNumber1) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (userNumber1 == winNumber1 || userNumber1 == winNumber2) {
+        println("Вы выиграли утешительный приз!")
+    } else if (userNumber2 == winNumber1 || userNumber2 == winNumber2) {
+        println("Вы выиграли утешительный приз!")
+    } else println("Неудача!")
+    println("Выйгрышные номера:$winNumber1,$winNumber2")
+}
