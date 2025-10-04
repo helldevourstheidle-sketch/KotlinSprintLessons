de82b821ab66eb7f3f1a09e20e3631ab8a794b17
AceVintura <helldevourstheidle@gmail.com>
2025-10-04 14:13:10 +0300

    KS-4-4 исправления

--- src/main/kotlin/lesson5/KS-1-5.kt
+++ src/main/kotlin/lesson5/KS-1-5.kt
@@ -1,6 +1,8 @@
 fun main() {
-    val rightAnswer = 5
-    println("Решите пример для подтверждения, что Вы не бот, введите ответ в консоль: 2+3=?")
+    val variable1 = 2
+    val variable2 = 3
+    val rightAnswer = variable1 + variable2
+    println("Решите пример для подтверждения, что Вы не бот, введите ответ в консоль: $variable1+$variable2=?")
     val userAnswer = readln().toInt()
     if (userAnswer == rightAnswer)
         println("Добро пожаловать!")
